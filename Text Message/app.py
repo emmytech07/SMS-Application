3cc56386d19d61bbc7184c213aa301ebb4e52429
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-13 06:07:16 +0100

    modifying changes

--- Text Message/app.py
+++ Text Message/app.py
@@ -4,7 +4,7 @@ from config import account_sid,  auth_token
 client = Client(account_sid, auth_token)
 
 client.messages.create(
-    to="+2348102122990",
+    to="...",
     from_="+14632403091",
     body = "I am checking on you. Automation"
 )
